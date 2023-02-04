57d9e32d4be3de49d9bed4106b88c7acdc5afd8b
mertensj <jan.jm.mertens@gmail.com>
2023-02-04 22:47:19 +0100

    updates from pt4

--- labwc/lab.sh
+++ labwc/lab.sh
@@ -7,17 +7,41 @@
 #sudo pacman -S libxml2 glib2
 #sudo pacman -S git
 #
+# == node pt4 ====
+# sudo pacman -S cmake
+# sudo pacman -S meson
+# sudo pacman -S wayland-protocols
+#
 #git clone https://github.com/labwc/labwc
 #cd labwc
 #meson setup build
 #ninja -C build
 #
+# seatd   not running - not able to get a seat
+#
+# sudo gpasswd -a jan seat
+# sudo systemctl start seatd
+# sudo systemctl enable seatd
+#
+# logout
+# login  -- to have user active in seat group
+# 
 
 # for compatibility
 #sudo pacman -S xorg-xwayland
 #
 
 #sudo pacman -S swaybg
+#sudo pacman -S terminator
+#
+#
+# install .config files for labwc / waybar
+#
+# git clone wallpaper
+#
+# for waybar install fonts:
+# sudo pacman -S otf-font-awesome
+#
 
-~/labwc/build/labwc -d -V
+~/git/labwc/build/labwc -d -V
 
